69835f60151ed3aacdbd1baaa33e81d2afcd692a
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-22 12:09:48 -0500

    completed labwork

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    new_list = []
+    for num in int_list:
+        new_num = num ** 2
+        new_list.append(new_num)
+    return new_list
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if (i % 5 == 0) and (i % 3 == 0):
+            print("FizzBuzz")
+        elif (i % 3 == 0):
+            print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
